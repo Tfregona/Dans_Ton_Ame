efa918fe488cda12e32f0cf2baa683b03a940c22
Tfregona <tristan.fregona@epitech.eu>
2022-03-30 16:41:46 +0200

    Update contact.js

--- pages/contact.js
+++ pages/contact.js
@@ -59,11 +59,11 @@ export default function Contact() {
               <label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2">
                 Description
               </label>
-              <p class="text-gray-600 text-xs italic">
-                Make it as long and as crazy as you'd like
+              <p className="text-gray-600 text-xs italic">
+                Précisez la raison de votre demande de contact
               </p>
               <textarea
-                className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
+                className="resize-none h-28 appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
                 id="grid-password"
                 type="text"
                 placeholder="Entrez ici"
