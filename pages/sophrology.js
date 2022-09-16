63f376c67a6abd643da0f782f933b55a187f3ec3
Tfregona <tristan.fregona@epitech.eu>
2022-09-16 17:32:18 +0200

    Update sophrology.js

--- pages/sophrology.js
+++ pages/sophrology.js
@@ -2,7 +2,7 @@ import ServicesCards from '../components/sophrology/cards'
 
 export default function Sophrology() {
   return (
-    <div className="">
+    <div>
       {/* Services */}
       <ServicesCards />
     </div>
